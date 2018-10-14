a65a9be35e0e86893a4ccf0186311ec2a46b541b
Aman <amandalmia18@gmail.com>
2018-10-15 00:32:23 +0530

    NEW: added checkpoint and results for Double DQN

--- 2 - Value-based methods/Project-Navigation/model.py
+++ 2 - Value-based methods/Project-Navigation/model.py
@@ -41,13 +41,13 @@ class DuelingQNetwork(nn.Module):
             fc1_units (int): Number of nodes in first hidden layer
             fc2_units (int): Number of nodes in second hidden layer
         """
-        super(QNetwork, self).__init__()
+        super(DuelingQNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         
         self.action_size = action_size
-        self.value_function_fc = nn.Linear(fc2_units, state_size)
+        self.value_function_fc = nn.Linear(fc2_units, 1)
         self.advantage_function_fc = nn.Linear(fc2_units, action_size)
 
     def forward(self, state):
